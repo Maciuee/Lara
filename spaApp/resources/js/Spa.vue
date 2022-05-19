41e11ba763e7dd7f5c496ef2d91eeb224a1f04f2
Maciuee <59364005+Maciuee@users.noreply.github.com>
2022-05-19 14:50:17 +0200

    login update

--- spaApp/resources/js/Spa.vue
+++ spaApp/resources/js/Spa.vue
@@ -1,12 +1,17 @@
 <template>
 <div class="container">
-Spa component<br>
-<router-link :to="{ name: 'home' }" >Home</router-link>|
-<router-link :to="{ name: 'login' }" >Login</router-link>|
-<router-link :to="{ name: 'dashboard' }" >Dashboard</router-link><br>
+Spa component
+<br />
+<navigation/>
 <router-view></router-view>
 </div>
 </template>
 <script>
- export default { }
-</script>
+import Navigation from
+"./components/Navigation.vue";
+export default {
+components: {
+Navigation
+}
+};
+</script> 
--- spaApp/resources/js/views/Login.vue
+++ spaApp/resources/js/views/Login.vue
@@ -1,8 +1,45 @@
 <template>
+<div class="m-3">
+Login component<br />
+<form>
 <div>
-Login component
+<label for="email">Email:</label>
+<input v-model="form.email" type="email" id="email" />
+</div>
+<div>
+<label for="password">Hasło:</label>
+<input v-model="form.password" type="password" id="password" />
+</div>
+<button @click.prevent="login">Zaloguj się</button>
+</form>
 </div>
 </template>
 <script>
-export default { }
+export default {
+data() {
+return {
+form: {
+email: "",
+password: ""
+}
+};
+},
+methods: { login() {console.log("Będzie logowanie tutaj!")},
+
+async login() {
+try {
+await axios.get("/sanctum/csrf-cookie");
+await axios.post("/api/login", {
+email: this.form.email,
+password: this.form.password
+});
+this.$router.push({ name: "dashboard" });
+} catch (err) {
+console.log(err);
+this.form.email = "";
+this.form.password = "";
+}
+}
+} };
 </script>
+
